97e972ab92341638251b03884488c22b6dd3145f
Loris PLANTÉ <lplante@lerebours.org>
2020-10-09 10:52:49 +0200

    Ajout d'une bibliothèque de classe Metier par LP

---
+++ MetierGit/Revue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetierGit
+{
+    public class Revue
+    {
+        private int numero;
+    }
+}
